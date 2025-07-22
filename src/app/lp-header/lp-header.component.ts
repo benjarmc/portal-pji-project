2ac19c10b53eb43ec1871cc3b1a497f5020c1295
AbisurDiazR <abisurdiazramirez@gmail.com>
2025-07-22 10:13:48 -0600

    Mejora de estilos

--- src/app/lp-header/lp-header.component.ts
+++ src/app/lp-header/lp-header.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Component,
+  HostListener,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
 
 @Component({
   selector: 'app-lp-header',
   imports: [],
   templateUrl: './lp-header.component.html',
-  styleUrl: './lp-header.component.scss'
+  styleUrl: './lp-header.component.scss',
 })
-export class LpHeaderComponent {
+export class LpHeaderComponent implements OnInit {
+  isNavbarShrunk: boolean = false;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.navbarScroll();
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.navbarScroll();
+    }
+  }
+
+  private navbarScroll() {
+    const navbar = document.querySelector('.navbar');
+    if (!navbar) {
+      return;
+    }
+
+    const scrollThreshold = 100;
+
+    if (window.scrollY > scrollThreshold) {
+      this.isNavbarShrunk = true;
+    } else {
+      this.isNavbarShrunk = false;
+    }
+  }
+
+  toggleNavbarShrink() {
+    if (window.scrollY > 100) {
+      this.isNavbarShrunk = !this.isNavbarShrunk;
+    }
+  }
 }
